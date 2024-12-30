0c7bd1df026c6c69740790517444e53f429a5b6c
Chayney <chunchunmaru33333@gmail.com>
2024-12-30 16:55:40 +0900

    会員登録ページの作成

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -62,7 +62,7 @@
         </div>
         <div class="form__group-content">
           <div class="form__input--text">
-            <input type="password" name="password" value="{{ old('password') }}">
+            <input type="password" name="password_confirmation">
           </div>
           <div class="form__error">
             @error('password')
