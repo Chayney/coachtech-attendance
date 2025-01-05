5a05d26431c667a04277441807ad501f176ac1c4
Chayney <chunchunmaru33333@gmail.com>
2025-01-05 14:43:07 +0900

    休憩登録処理

---
+++ src/app/Http/Controllers/RestController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Attendance;
+use App\Models\Rest;
+use Carbon\Carbon;
+
+class RestController extends Controller
+{
+    public function breakIn(Request $request)
+    {
+        $user = Auth::user();
+        $attend = Attendance::where('user_id', $user->id)->latest()->first();
+        Rest::create([
+            'attendance_id' => $attend->id,
+            'start_rest' => Carbon::now()
+        ]);
+        
+        return redirect('/attendance')->with('success', '休憩開始しました');   
+    }
+
+    public function breakOut(Request $request)
+    {
+        $user = Auth::user();
+        $attend = Attendance::where('user_id', $user->id)->latest()->first();
+        $rest = Rest::where('attendance_id', $attend->id)->latest()->first();
+        $rest->update([
+            'end_rest' => Carbon::now()
+        ]);
+        $resttime = Rest::selectRaw('SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(end_rest, start_rest)))) as totalresttime')->where('attendance_id', $attend->id)->first();
+        $attend->update([
+            'break_time' => $resttime->totalresttime
+        ]);
+
+        return redirect('/attendance')->with('success', '休憩終了しました');   
+    }
+}
