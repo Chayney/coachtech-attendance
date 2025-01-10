6d98b93d3ed769690dc1caa72751e7fac9fca511
Chayney <chunchunmaru33333@gmail.com>
2025-01-11 00:22:58 +0900

    ダミーデータの作成

---
+++ src/database/seeders/StatusesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => '勤務外'
+        ];
+        DB::table('statuses')->insert($param);
+
+        $param = [
+            'name' => '出勤中'
+        ];
+        DB::table('statuses')->insert($param);
+
+        $param = [
+            'name' => '休憩中'
+        ];
+        DB::table('statuses')->insert($param);
+
+        $param = [
+            'name' => '退勤済'
+        ];
+        DB::table('statuses')->insert($param);
+    }
+}
