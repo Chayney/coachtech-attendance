77855c3a50753c0c850756a0a680a47bac5cc17c
Chayney <chunchunmaru33333@gmail.com>
2025-01-26 23:27:12 +0900

    日時取得テスト

--- src/tests/Feature/DateTimeTest.php
+++ src/tests/Feature/DateTimeTest.php
@@ -5,7 +5,11 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\Status;
+use App\Models\Attendance;
+use Carbon\Carbon;
 
 class DateTimeTest extends TestCase
 {
@@ -16,11 +20,24 @@ class DateTimeTest extends TestCase
      */
     public function test_current_datetime()
     {
+        $this->seed(\Database\Seeders\StatusesTableSeeder::class);
+        $status = Status::where('name', '勤務外')->first();
         $user = User::factory()->create();
+        $today = Carbon::now()->toDateString();
+        Attendance::create([
+            'user_id' => $user->id,
+            'status_id' => $status->id,
+            'date' => $today,
+            'commute' => null
+        ]);
         $this->actingAs($user);
-        $now = now()->isoFormat("YYYY年MM月DD日(ddd)");
         $response = $this->get('/attendance');
-        $response->assertStatus(200)->assertSee($now);
+        $now = Carbon::now()->isoFormat("YYYY年MM月DD日(ddd)");
+        $response = $this->get('/attendance');
+        $this->assertAuthenticatedAs($user);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Status::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $user->delete();
     }
 }
