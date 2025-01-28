1a87a7a2ffb774bc70e58ab94ba34bd60a9f4df7
Chayney <chunchunmaru33333@gmail.com>
2025-01-28 23:29:40 +0900

    退勤テスト

--- src/tests/Feature/LeaveTest.php
+++ src/tests/Feature/LeaveTest.php
@@ -85,7 +85,7 @@ class LeaveTest extends TestCase
         $thisMonth = $currentMonth->format('Y/m');
         $lastMonth = $currentMonth->copy()->subMonth()->format('Y/m');
         $nextMonth = $currentMonth->copy()->addMonth()->format('Y/m');
-        $attendances = Attendance::with('approves')->where('user_id', $user->id)->whereRaw("DATE_FORMAT(date, '%Y/%m') = ?", [$thisMonth])->orderby('date', 'ASC')->get();
+        $attendances = Attendance::where('user_id', $user->id)->whereRaw("DATE_FORMAT(date, '%Y/%m') = ?", [$thisMonth])->orderby('date', 'ASC')->get();
         $this->assertTrue($attendances->contains('status_id', $endDutyStatus->id));
         $response = $this->get('/attendance/list');
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
