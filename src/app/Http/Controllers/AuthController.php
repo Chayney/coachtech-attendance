13b96cb0deedf4cfb346616240c14bbfe0c7c797
Chayney <chunchunmaru33333@gmail.com>
2025-02-02 20:13:13 +0900

    カスタムログイン設定

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->hasRole('admin')) {
+                return redirect()->intended('/admin/attendance/list');
+            }
+
+            return redirect()->intended('/attendance');
+        }
+
+        return back()->withInput($request->only('email'))->withErrors([
+            'password' => 'ログイン情報が登録されていません',
+        ]);
+    }
+}
