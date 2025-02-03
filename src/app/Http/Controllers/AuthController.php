1d9e52fc8162a9ec7530ed02f579c3d6cdc9d827
Chayney <chunchunmaru33333@gmail.com>
2025-02-03 19:36:52 +0900

    カスタムログイン設定

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
         }
 
         return back()->withInput($request->only('email'))->withErrors([
-            'password' => 'ログイン情報が登録されていません',
+            'email' => 'ログイン情報が登録されていません',
         ]);
     }
 }
